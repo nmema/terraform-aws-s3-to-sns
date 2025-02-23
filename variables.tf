625366f2b66689bf702fae8e98e2b06e6b32780f
Nicolas Mema <nicolas.a.mema@gmail.com>
2025-02-23 15:08:20 -0300

    #3 add SES

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "bucket_name" {
   description = "S3 Bucket name"
   type        = string
 }
+
+variable "email" {
+  description = "Email to use for Amazon Simple Email Service"
+  type        = string
+}
