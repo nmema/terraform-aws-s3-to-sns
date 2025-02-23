dfd7a802a8defe9907ca64ab38785dca01d84b76
Nicolas Mema <nicolas.a.mema@gmail.com>
2025-02-23 15:59:06 -0300

    #5 add dependency

--- s3.tf
+++ s3.tf
@@ -40,6 +40,14 @@ resource "aws_s3_bucket_policy" "require_tags" {
   })
 }
 
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.repo.arn
+}
+
 resource "aws_s3_bucket_notification" "s3_event_notification" {
   bucket = aws_s3_bucket.repo.id
 
@@ -48,12 +56,6 @@ resource "aws_s3_bucket_notification" "s3_event_notification" {
     lambda_function_arn = aws_lambda_function.lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
-}
 
-resource "aws_lambda_permission" "allow_s3" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.repo.arn
+  depends_on = [aws_lambda_permission.allow_s3]
 }
