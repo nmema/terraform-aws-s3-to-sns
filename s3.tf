e619ea237b3e743e423b397d4cf6468595039635
Nicolas Mema <nicolas.a.mema@gmail.com>
2025-02-23 11:36:47 -0300

    #1 add lambda trigger event

--- s3.tf
+++ s3.tf
@@ -52,3 +52,21 @@ resource "aws_s3_bucket_policy" "require_tags" {
     ]
   })
 }
+
+resource "aws_s3_bucket_notification" "s3_event_notification" {
+  bucket = aws_s3_bucket.repo.id
+
+  lambda_function {
+    id                  = "CreationTrigger"
+    lambda_function_arn = aws_lambda_function.lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.repo.arn
+}
